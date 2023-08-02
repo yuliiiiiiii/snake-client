a8ed9e307d6608969f5563b28f4e6848a84f2386
Yuli Wang <yuliwang1007@gmail.com>
2023-08-02 15:54:47 -0400

    refactured using require and module.export

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require('net');
+// const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-  //create a connection with a server using ip and port
-
-  conn.setEncoding('utf-8');
-  conn.on('data', data => {
-    console.log(data);
-  });
-    
-  return conn; //the connection i have with the server
-};
+const connect = require('./client');
 
 console.log('Connecting...');
 connect();
