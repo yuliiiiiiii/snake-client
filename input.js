dfe5e4a347e49856cdfd6a86ac247b6ca4092c86
Yuli Wang <yuliwang1007@gmail.com>
2023-08-02 19:45:47 -0400

    link input to server

--- input.js
+++ input.js
@@ -1,8 +1,12 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf-8');
   stdin.resume();
+  
+  connection = conn;
 
   stdin.on('data', handleUserInput)
   return stdin; //allow me to listen for keyboard unput and react to it - i can see what i did for my input
@@ -12,7 +16,20 @@ const handleUserInput = function(key) {
   if(key === '\u0003') { //check for the ctrl + C input and terminate the game
     process.exit();
   }
+  if(key === "w") {
+    connection.write("Move: up");
+  }
+  if(key === "a") {
+    connection.write("Move: left");
+  }
+  if(key === "s") {
+    connection.write("Move: down");
+  }
+  if(key === "d") {
+    connection.write("Move: right");
+  }
 }
+
 
 module.exports = setupInput;
 //No need to export handleUserInput, because it is only called within this file.
--- play.js
+++ play.js
@@ -5,6 +5,6 @@ const connect = require('./client');
 const setupInput = require('./input');
 
 console.log('Connecting...');
-connect();
+// connect();
 
-setupInput();
+setupInput(connect());
