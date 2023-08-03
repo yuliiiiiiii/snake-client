75b18ce738d97ac9ff97a68d5cb960e83e9a785b
Yuli Wang <yuliwang1007@gmail.com>
2023-08-02 21:38:42 -0400

    crated constants

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net');
 
+const { IP, PORT } = require('./constants');
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   //create a connection with a server using ip and port
   conn.setEncoding('utf-8');
